a169c158ebb041b0e2d0e0660bc07c6688df6668
East <165237258+FUADIKAMIL@users.noreply.github.com>
2025-10-24 01:10:41 +0700

    Adjust main container margin in log layout

--- resources/views/layouts/log.blade.php
+++ resources/views/layouts/log.blade.php
@@ -7,7 +7,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-cover bg-center bg-no-repeat min-h-screen text-gray-800 flex items-center justify-center" style="background-image: url('{{ asset('images/bg5.png') }}')">
-    <main class="bg-white bg-opacity-80 backdrop-blur-md rounded-2xl shadow-lg p-6 max-w-md w-full mx-auto">
+    <main class="bg-white bg-opacity-80 backdrop-blur-md rounded-2xl shadow-lg p-6 max-w-md w-full mx-5">
         @yield('content')
     </main>
 </body>
