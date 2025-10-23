4ba4ea656f7db49a3c0bc985d535946375c6221a
East <165237258+FUADIKAMIL@users.noreply.github.com>
2025-10-24 01:08:06 +0700

    Adjust padding in login form and fix formatting

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,13 +4,13 @@
 <div>
     <img src="{{ asset('images/logo.png') }}" alt="Profile" class="w-120">
 </div>
-<div class="max-w-md mx-auto bg-white p-6 rounded-2xl mt-[-25px]">
+<div class="max-w-md mx-auto bg-white p-2 rounded-2xl mt-[-25px]">
     <h2 class="text-2xl font-bold text-center mb-4">Temukan Shunya-mu</h2>
     <form method="POST" action="/login" class="space-y-4">
         @csrf
         <div>
             <label class="block text-sm font-medium">Username</label>
-            <input type="text" name="username"  placeholder="Nama kamu" class="w-full border rounded p-2" required>
+            <input type="text" name="username" placeholder="Nama kamu" class="w-full border rounded p-2" required>
         </div>
         <div>
             <label class="block text-sm font-medium">Password</label>
