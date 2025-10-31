e52f0d6295073dc2bf9f662bcfd5ee7a7b3412c1
East <165237258+FUADIKAMIL@users.noreply.github.com>
2025-10-31 17:05:33 +0700

    Update katalog.blade.php

--- resources/views/katalog.blade.php
+++ resources/views/katalog.blade.php
@@ -10,8 +10,8 @@
                 <img src="{{ asset($item['gambar']) }}" alt="{{ $item['judul'] }}" class="object-cover rounded-md mb-3">
             </a>
             <h3 class="text-lg font-semibold">{{ $item['judul'] }}</h3>
+            <p class="text-sm text-gray-500 mb-1 text-justify">Oleh: {{ $item['oleh'] }}</p>
             <p class="text-sm text-gray-500 mb-6 text-justify">{{ $item['desk'] }}</p>
-            <p class="text-sm text-gray-500 mb-6 text-justify">Oleh: {{ $item['oleh'] }}</p>
             <div class="absolute bottom-0 left-0 p-4">
                 <span class="text-xs bg-red-100 text-red-700 px-2 py-1 rounded-md">{{ $item['bidang'] }}</span>
             </div>
@@ -20,7 +20,6 @@
 </div>
 
 @endsection
-
 
 
 
